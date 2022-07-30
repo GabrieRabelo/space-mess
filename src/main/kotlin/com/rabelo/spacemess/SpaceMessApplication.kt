8dd4c1cc70d100fc73d6d216ae26ccf2ba74aa6b
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2022-07-30 15:05:44 -0300

    first commit

---
+++ src/main/kotlin/com/rabelo/spacemess/SpaceMessApplication.kt
@@ -0,0 +1,11 @@
+package com.rabelo.spacemess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpaceMessApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpaceMessApplication>(*args)
+}
---
+++ src/test/kotlin/com/rabelo/spacemess/SpaceMessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rabelo.spacemess
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpaceMessApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
